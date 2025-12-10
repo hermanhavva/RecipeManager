2f0fe19424b63f7978aa5248b753790b743e5ea4
Anna Oleksandrovych <150846802+anwalv@users.noreply.github.com>
2025-12-10 17:11:29 +0200

    fix: changed save method to add and added id for cart repo

--- Sources/Application/Protocols/CartRepositoryType.swift
+++ Sources/Application/Protocols/CartRepositoryType.swift
@@ -2,9 +2,9 @@ import Foundation
 import Domain
 
 public protocol CartRepositoryType {
-    func getItems() async throws -> [Ingredient]
-    func add(ingredients: [Ingredient]) async throws
-    func add(ingredient: Ingredient) async throws
-    func remove(id: UUID) async throws
-    func clear() async throws
+    func getItems(cartId: UUID) async throws -> [Ingredient]
+    func add(ingredients: [Ingredient], to cartId: UUID) async throws
+    func add(ingredient: Ingredient, to cartId: UUID) async throws
+    func remove(id: UUID, from cartId: UUID) async throws
+    func clear(cartId: UUID) async throws
 }
--- Sources/Application/Protocols/RecipeRepositoryType.swift
+++ Sources/Application/Protocols/RecipeRepositoryType.swift
@@ -3,7 +3,7 @@ import Domain
 
 public protocol RecipeRepositoryType {
     func fetchRecipes() async throws -> [Recipe]
-    func save(recipe: Recipe) async throws
+    func add(recipe: Recipe) async throws
     func update(recipe: Recipe) async throws
     func delete(recipeId: UUID) async throws
 }
